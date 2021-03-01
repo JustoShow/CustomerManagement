86db18ad169b2befafdc279a57718e0e538720b6
Justin Allen <justoproductions@yahoo.com>
2021-03-01 14:49:16 -0600

    Added Name/Phone Fields

--- CustomerManagement.DAL/Models/Email.cs
+++ CustomerManagement.DAL/Models/Email.cs
@@ -7,11 +7,15 @@ namespace CustomerManagement.DAL.Models
     public class Email
     {
         public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string EmailAddress { get; set; }
+        public string Title { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime? UpdatedTime { get; set; }
 
         public int CustomerID { get; set; }
         public Customer Customer { get; set; }
+        public ICollection<Phone> Phone { get; set; }
     }
 }

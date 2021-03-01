0b306ab178891d131bac0e42486328ed02bccaf7
Justin Allen <justoproductions@yahoo.com>
2021-03-01 14:49:30 -0600

    Added Phone Entity

---
+++ CustomerManagement.DAL/Models/Phone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerManagement.DAL.Models
+{
+    public class Phone
+    {
+        public int ID { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public int EmailID { get; set; }
+        public Email Email { get; set; }
+    }
+}

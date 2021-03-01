eee8ea32b7c47c0f1f4ca7fb71b9d501c75d7597
Justin Allen <justoproductions@yahoo.com>
2021-03-01 14:48:54 -0600

    Added Address/Mailing Fields

--- CustomerManagement.DAL/Models/Customer.cs
+++ CustomerManagement.DAL/Models/Customer.cs
@@ -8,6 +8,14 @@ namespace CustomerManagement.DAL.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? ZipCode { get; set; }
+        public string? MailAddress { get; set; }
+        public string? MailCity { get; set; }
+        public string? MailState { get; set; }
+        public string? MailZipCode { get; set; }
 
         public ICollection<Email> Emails { get; set; }
     }

6ff7dd38dcb2fc8abe8dadf94c48b81a72780559
Justin Allen <justoproductions@yahoo.com>
2021-03-06 06:41:18 -0600

    Adding Email Management

---
+++ CustomerManagement.UI.Blazor/Pages/Manage/Emails.razor.cs
@@ -0,0 +1,78 @@
+﻿using CustomerManagement.DAL.Data;
+using CustomerManagement.DAL.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.UI.Blazor.Pages.Manage
+{
+    public partial class Emails
+    {
+        [Inject]
+        private CMDbContext _context { get; set; }
+        [Inject]
+        private IDialogService DialogService { get; set; }
+        private List<Customer> CustomerList { get; set; }
+        private List<Email> EmailList { get; set; }
+        private Customer EmailDetails { get; set; } = new Customer();
+        private string searchString = "";
+        private int selectedCustomerId = 0;
+        private bool _hasCustomers = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _hasCustomers = _context.Customers.Any();
+
+            if (_hasCustomers)
+            {
+                await LoadCustomersAsync();
+            }
+        }
+
+        public async Task LoadCustomersAsync()
+        {
+            try
+            {
+                CustomerList = await _context.Customers.OrderBy(c => c.Name).ToListAsync();
+            }
+            catch (DbUpdateException)
+            {
+
+                return;
+            }
+        }
+
+        private async Task CustomerChanged(int customerId)
+        {
+            selectedCustomerId = customerId;
+            await LoadEmailsAsync(customerId);
+        }
+
+        private async Task LoadEmailsAsync(int customerId)
+        {
+            await _context.Emails.Where(e => e.CustomerID == customerId).OrderBy(e => e.EmailAddress).ToListAsync();
+        }
+
+        DialogOptions options = new DialogOptions() { DisableBackdropClick = true };
+
+        // Add Email to Customer
+        private async Task OpenEmailDialog()
+        {
+            // TODO: Finish Adding Email
+            var parameter = new DialogParameters { ["CustomerId"] = selectedCustomerId, ["IsNewEmail"] = true };
+            var dialog = DialogService.Show<CustomersDialog>("Add Email", parameter, options);
+
+            var result = await dialog.Result;
+
+            if (!result.Cancelled)
+            {
+                // Reload after added
+                await LoadCustomersAsync();
+            }
+        }
+    }
+}
---
+++ CustomerManagement.UI.Blazor/Pages/Manage/EmailsDialog.razor.cs
@@ -0,0 +1,83 @@
+﻿using CustomerManagement.DAL.Data;
+using CustomerManagement.DAL.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.UI.Blazor.Pages.Manage
+{
+    public partial class EmailsDialog
+    {
+        [Inject]
+        public CMDbContext _context { get; set; }
+        [CascadingParameter]
+        private MudDialogInstance MudDialog { get; set; }
+        [Inject]
+        private ISnackbar Snackbar { get; set; }
+        [Parameter]
+        public Email EmailDetails { get; set; } = new Email();
+        [Parameter]
+        public bool IsNewEmail { get; set; } = true;
+        [Parameter]
+        public int CustomerId { get; set; } = 0;
+
+        public async Task Submit(Email emailDetails)
+        {
+            Snackbar.Configuration.PositionClass = Defaults.Classes.Position.BottomRight;
+            if (IsNewEmail)
+            {
+                await AddCustomer(emailDetails);
+            }
+            else
+            {
+                await EditEmail(emailDetails);
+            }
+        }
+
+        public void Cancel() => MudDialog.Cancel();
+
+        public async Task AddCustomer(Email newEmail)
+        {
+            bool emailExist = _context.Emails.Any(c => c.EmailAddress == newEmail.EmailAddress);
+
+            if (!emailExist)
+            {
+                try
+                {
+                    // TODO: Add Email
+                    //await _context.SaveChangesAsync();
+                    Snackbar.Add("Customer has been added!", Severity.Success);
+                    MudDialog.Close(DialogResult.Ok(true));
+                    return;
+                }
+                catch (DbUpdateException)
+                {
+                    Snackbar.Add("Error adding customer to the database!", Severity.Error);
+                    return;
+                }
+            }
+            Snackbar.Add("Customer is already in the database!", Severity.Error);
+        }
+
+        public async Task EditEmail(Email email)
+        {
+            try
+            {
+                // TODO: Update Email
+                //await _context.SaveChangesAsync();
+                Snackbar.Add("Customer has been updated!", Severity.Success);
+                MudDialog.Close(DialogResult.Ok(true));
+                return;
+            }
+            catch (DbUpdateException)
+            {
+                Snackbar.Add("Error adding customer to the database!", Severity.Error);
+                return;
+            }
+        }
+    }
+}
